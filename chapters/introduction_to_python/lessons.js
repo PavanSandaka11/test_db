3cb6988fe9193ce7621ae66128e23bcf2c92fb30
PavanSandaka <pavansandaka11@gmail.com>
2025-06-22 20:03:24 +0000

    Update lesson: Installing Python

--- chapters/introduction_to_python/lessons.js
+++ chapters/introduction_to_python/lessons.js
@@ -3,6 +3,16 @@
   "chapter": "introduction_to_python",
   "lessons": [
     {
+      "title": "Installing Python",
+      "slug": "installing-python",
+      "file": "00-installing-python.md",
+      "lesson_order": 0,
+      "duration": "00:10:00",
+      "difficulty": "Beginner",
+      "prerequisites": [],
+      "objectives": []
+    },
+    {
       "title": "What is Python and Why Learn It?",
       "slug": "what-is-python-and-why-learn-it",
       "file": "00-what-is-python-and-why-learn-it.md",
