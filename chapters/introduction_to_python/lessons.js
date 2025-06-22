d5addc2fab8181ed20ce104b191f50b3f805a2b6
PavanSandaka <pavansandaka11@gmail.com>
2025-06-22 20:19:06 +0000

    Update lesson: What is Python and Why Learn It?

--- chapters/introduction_to_python/lessons.js
+++ chapters/introduction_to_python/lessons.js
@@ -3,36 +3,6 @@
   "chapter": "introduction_to_python",
   "lessons": [
     {
-      "title": "test lesson sync",
-      "slug": "test-lesson-sync",
-      "file": "00-test-lesson-sync.md",
-      "lesson_order": 0,
-      "duration": "00:10:00",
-      "difficulty": "Beginner",
-      "prerequisites": [],
-      "objectives": []
-    },
-    {
-      "title": "Installing Python",
-      "slug": "installing-python",
-      "file": "00-installing-python.md",
-      "lesson_order": 0,
-      "duration": "00:10:00",
-      "difficulty": "Beginner",
-      "prerequisites": [],
-      "objectives": []
-    },
-    {
-      "title": "Your First Python Program",
-      "slug": "your-first-python-program",
-      "file": "00-your-first-python-program.md",
-      "lesson_order": 0,
-      "duration": "00:10:00",
-      "difficulty": "Beginner",
-      "prerequisites": [],
-      "objectives": []
-    },
-    {
       "title": "What is Python and Why Learn It?",
       "slug": "what-is-python-and-why-learn-it",
       "file": "00-what-is-python-and-why-learn-it.md",
